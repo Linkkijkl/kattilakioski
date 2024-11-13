514290121935a83351935b05ba57ce4f3e12b834
Lari Nurkka <lari.nurkka@gmail.com>
2024-11-13 17:11:00 +0200

    Initial project skeleton

---
+++ src/api.rs
@@ -0,0 +1,14 @@
+use crate::BB8Pool;
+use actix_web::{get, web, Error, HttpResponse};
+
+pub fn config(cfg: &mut web::ServiceConfig) {
+    cfg.service(
+        web::scope("/api")
+            .service(hello_world)
+    );
+}
+
+#[get("/hello")]
+pub async fn hello_world(_path: web::Path<String>, _pool: web::Data<BB8Pool>) -> Result<HttpResponse, Error> {
+    Ok(HttpResponse::Ok().body("Hello World!"))
+}
---
+++ src/main.rs
@@ -0,0 +1,40 @@
+use std::io::ErrorKind;
+use actix_files::Files;
+use actix_web::{middleware, web, App, HttpServer};
+use diesel_async::pooled_connection::bb8::Pool;
+use diesel_async::pooled_connection::AsyncDieselConnectionManager;
+
+pub type BB8Pool = Pool<diesel_async::AsyncPgConnection>;
+
+mod api;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    // TODO: More secure logging
+    pretty_env_logger::init();
+
+    let diesel_connection_manager =
+        AsyncDieselConnectionManager::<diesel_async::AsyncPgConnection>::new(
+            std::env::var("DATABASE_URL").unwrap_or("postgres://postgres".to_string()),
+        );
+    let diesel_connection_pool: BB8Pool = Pool::builder()
+        .build(diesel_connection_manager)
+        .await
+        .map_err(|_| {
+            std::io::Error::new(ErrorKind::ConnectionRefused, "Can't connect to Postgres")
+        })?;
+
+    HttpServer::new(move || {
+        App::new()
+            .app_data(web::Data::new(diesel_connection_pool.clone()))
+            .wrap(
+                middleware::DefaultHeaders::new().add(("content-type", "text/html; charset=UTF-8")),
+            )
+            .wrap(middleware::Logger::new("%t %s %r %Dms"))
+            .configure(api::config)
+            .service(Files::new("/", "public").index_file("index.html"))
+    })
+    .bind(("0.0.0.0", 3030))?
+    .run()
+    .await
+}
