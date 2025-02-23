6e661291d3c5b358d09de64daa3d3448bdc16b10
Lari Nurkka <lari.nurkka@gmail.com>
2025-02-23 12:07:58 +0000

    Test runner script

---
+++ test-runner/tests.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Build backend
+cargo build || exit 1
+# Start backend server in background 
+cargo run &
+sleep 0.5
+# Store backend process id for later use
+BACKEND_PID=$!
+cargo test -- --test-threads 1 || exit 1
+# Kill backend server
+kill $BACKEND_PID
+
+# Check frontend code
+yarn run check || exit 1
