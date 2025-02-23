288d06e4215452dd42a515afcab849d40b0d9114
Lari Nurkka <lari.nurkka@gmail.com>
2025-02-23 19:05:11 +0000

    Fixed broken frontend

--- tests.sh
+++ tests.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Check that database is reachable
+# Check if database is reachable
 pg_isready -h postgres || exit 1
 
 # Build backend
@@ -11,7 +11,7 @@ sleep 0.5
 # Store backend process id for later use
 BACKEND_PID=$!
 cargo test -- --test-threads 1 || exit 1
-# Kill backend server
+# Stop backend server
 kill $BACKEND_PID
 
 # Check frontend
