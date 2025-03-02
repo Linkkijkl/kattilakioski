6c1d82dd27bc39a6bb0ad7efe4dd4d2330d607f7
Lari Nurkka <lari.nurkka@gmail.com>
2025-03-02 19:35:01 +0200

    Fixed bad database migrations (#10)

--- migrations/2025-02-27-140510_append_seller_to_transactions/up.sql
+++ migrations/2025-02-27-140510_append_seller_to_transactions/up.sql
@@ -1,5 +1,5 @@
 ALTER TABLE transactions RENAME COLUMN buyer_id TO payer_id;
-ALTER TABLE transactions ADD COLUMN IF NOT EXISTS receiver_id INTEGER REFERENCES users(id) NOT NULL;
+ALTER TABLE transactions ADD COLUMN IF NOT EXISTS receiver_id INTEGER REFERENCES users(id);
 ALTER TABLE transactions ALTER COLUMN item_id DROP NOT NULL;
 ALTER TABLE transactions ALTER COLUMN payer_id DROP NOT NULL;
 ALTER TABLE transactions ALTER COLUMN item_amount SET DEFAULT 0;
@@ -12,3 +12,8 @@ UPDATE transactions t
 SET receiver_id = i.seller_id
 FROM items i
 WHERE t.item_id = i.id;
+
+/*
+Make receiver id non-nullable *after* transferring data, to keep relation intact
+*/
+ALTER TABLE transactions ALTER COLUMN receiver_id SET NOT NULL;
