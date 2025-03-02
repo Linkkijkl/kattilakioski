14732e7ff2f368825c9bf8624a0b107601be2e31
Lari Nurkka <lari.nurkka@gmail.com>
2025-03-02 17:47:36 +0000

    Fixed bad db migrations

--- migrations/2025-02-27-214549_append_amount_to_transactions/up.sql
+++ migrations/2025-02-27-214549_append_amount_to_transactions/up.sql
@@ -1,4 +1,4 @@
-ALTER TABLE transactions ADD COLUMN IF NOT EXISTS amount_cents INTEGER NOT NULL;
+ALTER TABLE transactions ADD COLUMN IF NOT EXISTS amount_cents INTEGER;
 
 /*
 Transfer newly created required column `amount_cents` from associated item (appointed by `item_id`,
@@ -8,3 +8,8 @@ UPDATE transactions t
 SET amount_cents = i.price_cents
 FROM items i
 WHERE t.item_id = i.id;
+
+/*
+Make make transaction amount non-nullable *after* transferring data, to keep relation intact
+*/
+ALTER TABLE transactions ALTER COLUMN amount_cents SET NOT NULL;
